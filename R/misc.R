aa3ef153ad74124ba39ed1358de2d109e7950785
Maximilian Rohde <maxdrohde@gmail.com>
2025-02-12 11:56:52 -0800

    new

--- R/misc.R
+++ R/misc.R
@@ -7,3 +7,19 @@ check_mcmc <- function(mcmc_output){
   MCMCvis::MCMCtrace(mcmc_output)
   MCMCvis::MCMCplot(mcmc_output)
 }
+
+#' Extract MCMC summary
+#' @export
+mcmc_summary <- function(mcmc_output,
+                         dataset_id){
+
+  out <-
+  mcmc_output |>
+    MCMCvis::MCMCsummary() |>
+    as.data.frame() |>
+    tibble::rownames_to_column(var = "parameter")
+
+  out$dataset_id <- dataset_id
+
+  return(out)
+}
