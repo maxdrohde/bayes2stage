61c70f00776db58aa0391bb5ac28133d95c7ac16
Maximilian Rohde <maxdrohde@gmail.com>
2025-12-05 02:30:47 -0800

    new

--- R/fit_stan.R
+++ R/fit_stan.R
@@ -1,7 +1,7 @@
 mod1 <- cmdstanr::cmdstan_model("~/r_package_development/bayes2stage/inst/stan_models/mixed_effects_imputation.stan")
 mod2 <- cmdstanr::cmdstan_model("~/r_package_development/bayes2stage/inst/stan_models/mixed_effects_imputation_centered.stan")
 
-df <- bayes2stage::generate_data(N = 500,
+df <- bayes2stage::generate_data(N = 300,
                                  x_dist = "normal") |>
   bayes2stage::srs_design(N = 100)
 
@@ -14,20 +14,37 @@ fit1 <- mod1$sample(
   seed = 1234,
   chains = 10,
   parallel_chains = 10,
-  iter_warmup = 500,
-  iter_sampling = 500,
+  iter_warmup = 2000,
+  iter_sampling = 2000,
   adapt_delta = 0.99
 )
+
+fit2 <- mod1$sample(
+  data = data_list,
+  seed = 1234,
+  chains = 10,
+  parallel_chains = 10,
+  iter_warmup = 2000,
+  iter_sampling = 2000
+)
+
+fit1_draws <-
+  fit1$draws(variables = c("alpha_main", "beta_t", "beta_x", "beta_x_t_interaction",
+                          "beta", "sigma_main", "sigma_re", "corr_rand_effects"))
 
 fit2 <- mod2$sample(
   data = data_list,
   seed = 1234,
   chains = 10,
   parallel_chains = 10,
-  iter_warmup = 2000,
-  iter_sampling = 2000,
+  iter_warmup = 500,
+  iter_sampling = 500,
   adapt_delta = 0.99
 )
+
+fit2_draws <-
+  fit1$draws(variables = c("alpha_main", "beta_t", "beta_x", "beta_x_t_interaction",
+                           "beta", "sigma_main", "sigma_re", "corr_rand_effects"))
 
   posterior::as_draws(fit1) |>
   posterior::subset_draws(variable = c("beta_x", "beta[1]", "beta_t")) |>
